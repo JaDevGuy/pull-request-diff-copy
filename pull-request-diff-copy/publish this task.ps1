71256ef5fa717003e47e7f059a2c42f4b502002d
lixiaoming <liminany@gmail.com>
2018-03-29 12:44:04 +0800

    update cd script

--- pull-request-diff-copy/publish this task.ps1
+++ pull-request-diff-copy/publish this task.ps1
@@ -1,3 +1,2 @@
-#tfx extension publish --token $(MarketplaceExtPAT) --manifest-globs vss-extension.json --publisher lean-soft
+tfx extension publish --token $(MarketplaceExtPAT) --manifest-globs vss-extension.json --publisher lean-soft
 # --rev-version
-tfx extension publish --token icmdj6dwlvj2zvkgcjiggytcqxi2cqijjv3fdzgmp2vikd4l5qvq --manifest-globs vss-extension.json --publisher lean-soft
